597e8bfd3c0178fcbe27648486b503e90d10a4f7
NikoKS <nikolas.kur@gmail.com>
2022-02-08 13:48:10 +0700

    change tmux border color

--- dotfiles/lvim/lua/tree.lua
+++ dotfiles/lvim/lua/tree.lua
@@ -6,6 +6,7 @@ lvim.builtin.nvimtree.setup.hijack_cursor = true
 lvim.builtin.nvimtree.setup.update_cwd = true
 lvim.builtin.nvimtree.setup.update_focused_file.update_cwd = true
 lvim.builtin.nvimtree.setup.diagnostics.enable = false
+lvim.builtin.nvimtree.setup.auto_close = true
 local tree_cb = require'nvim-tree.config'.nvim_tree_callback
 lvim.builtin.nvimtree.setup.view.mappings.list = {
   { key = {"<space>"}, cb = tree_cb("edit") },
