fa41989689eead537e89c562ea9a45966e59421e
NikoKS <nikolas.kur@gmail.com>
2023-06-17 09:44:10 +0700

    refactor installation

--- install.sh
+++ install.sh
@@ -1,51 +1,33 @@
 #!/usr/bin/env bash
 
-# Script to install dotfiles and environment on system.
-# Inspired from: https://github.com/atomantic/dotfiles
-# Depending if you're on a personal or remote machine, this scirpt would install
-# A full or lite version
-
 # always run from the script dir
-cd "${0%/*}"
-
-# register TERM signal to exit
-trap "exit 1" TERM
-export TOP_PID=$$
+cd "${0%/*}" || return
 
 # Source library
-source ./lib/echos.sh
-source ./lib/requires.sh
 source ./lib/installs.sh
 source ./lib/core.sh
 
-function turn_off() {
-  bot "Goodbye"
-  kill -s TERM $TOP_PID
-}
-
-bot "Hi! I'm going to install tools and tweak your system settings. Here I go..."
-
 if [[ $SSH_TTY ]]; then
-  info "Seems that you're seting up a remote machine!"
-  bot "First I would need your password to run sudo commands"
-  sudo -v
-  distro=`cat /etc/*-release | grep '^ID=' | cut -d = -f2 | sed 's/"//g'`
-  if [[ $distro == 'centos' ]]; then
-    get_software_centos
-  else [[ $distro == 'debian' ]]
-    get_software_ubuntu
-  fi
-  copy_dotfiles
-  install_plugins
+	# For linux
+	software=$HOME/Software
+	sudo -v
+	sudo apt update && sudo apt -y upgrade
+	install_pack 'apt install -y' ./packages/apt.pack
+	install_pack 'pip install' ./packages/pip-remote.pack
+	install_pack 'npm i -g' ./packages/npm.pack
+	install_pack 'cargo install' ./packages/cargo.pack
+	install_nvim_appimage "$software"
+	install_tmux_appimage "$software"
+	link_dotfiles
+	install_tmux_plugin
+	install_zsh_plugin
 else
-  info "Seems that you're seting up your main mac machine!"
-  go_passwordless
-  update_hosts
-  install_brew
-  install_brew_packages
-  install_brew_casks
-  link_dotfiles
-  install_plugins
+	# For Macos
+	go_passwordless
+	# update_hosts (to be implemented)
+	install_brew
+	install_pack 'brew install' ./packages/brew.pack
+	install_pack 'brew install --cask' ./packages/cask.pack
+	install_pack 'npm i -g' ./packages/npm.pack
+	link_dotfiles
 fi
-
-ok "Done setting up"
--- lib/installs.sh
+++ lib/installs.sh
@@ -1,158 +1,76 @@
 # Compilation of script to install software from source
 
-function version_greater_equal() {
-  printf '%s\n' "$2" "$1" | sort -V -C
-}
-
-function clone_or_pull() {
-  if [[ -d $1 ]]; then
-    info "$1 exist, updating"
-    pushd $1 > /dev/null
-    git pull --ff-only
-    popd > /dev/null
-  else
-    info "cloning $2 to $1"
-    git clone --depth=1 $2 $1
-  fi
-}
-
-# Install poetry
-
-function install_poetry() {
-  curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -
-}
-
-# install lunarvim rolling branch
-function install_lvim_rolling() {
-  require_package nodejs
-  require_package python3
-  require_package cargo
-  require_pip3 pynvim
-  NPM_PACKAGES="$HOME/.npm-packages"
-  mkdir -p "$NPM_PACKAGES"
-  echo "prefix = $NPM_PACKAGES" >> ~/.npmrc
-  LV_BRANCH=rolling bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/rolling/utils/installer/install.sh)
-}
-
-function install_lvim_stable() {
-  require_package nodejs
-  require_package python3
-  require_package cargo
-  require_pip3 pynvim
-  NPM_PACKAGES="$HOME/.npm-packages"
-  mkdir -p "$NPM_PACKAGES"
-  echo "prefix = $NPM_PACKAGES" >> ~/.npmrc
-  bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh)
-}
+local_bin=$HOME/.local/bin
+config_dir=$HOME/.config
+plugins_dir=$config_dir/tmux/plugins
+zsh_dir=$config_dir/zsh
 
 # install latest nvim appimage
 # argument: nvim install directory
 function install_nvim_appimage() {
-  mkdir -p $1
-  info "installing nvim in $1"
-  pushd $1
-  lastversion --asset neovim/neovim --filter appimage$ -d nvim.AppImage
-  chmod u+x nvim.AppImage
-  ./nvim.AppImage --appimage-extract 1> /dev/null
-  info "linking nvim binary to local bin"
-  local_bin=$HOME/.local/bin
-  mkdir -p $local_bin
-  ln -fs `pwd`/squashfs-root/AppRun $local_bin/nvim
-  popd
-  ok
+	mkdir -p "$1"
+	pushd "$1" || return
+	lastversion --asset neovim/neovim --filter appimage$ -d nvim.AppImage
+	chmod u+x nvim.AppImage
+	./nvim.AppImage --appimage-extract 1>/dev/null
+	mkdir -p "$local_bin"
+	ln -fs "$(pwd)"/squashfs-root/AppRun "$local_bin"/nvim
+	popd || return
 }
 
 # install latest tmux appimage
 # argument: tmux install directory
 function install_tmux_appimage() {
-  mkdir -p $1
-  info "installing tmux in $1"
-  pushd $1
-  lastversion tmux --pre --having-asset "~\.AppImage" --assets --filter AppImage -d tmux.AppImage
-  chmod u+x tmux.AppImage
-  ./tmux.AppImage --appimage-extract 1> /dev/null
-  info "linking tmux binary to local bin"
-  local_bin=$HOME/.local/bin
-  mkdir -p $local_bin
-  ln -fs `pwd`/squashfs-root/AppRun $local_bin/tmux
-  popd
-  ok
+	mkdir -p "$1"
+	pushd "$1" || return
+	lastversion tmux --pre --having-asset "~\.AppImage" --assets --filter AppImage -d tmux.AppImage
+	chmod u+x tmux.AppImage
+	./tmux.AppImage --appimage-extract 1>/dev/null
+	mkdir -p "$local_bin"
+	ln -fs "$(pwd)"/squashfs-root/AppRun "$local_bin"/tmux
+	popd || return
+}
+
+# install zsh latest from source (NOT WORKING: cannot compile from github source)
+# argument: zsh install directory
+function install_zsh_source() {
+	mkdir -p "$1"
+	info "installing zsh in $1"
+	pushd "$1" || return
+	require_yum ncurses-devel
+	lastversion --assets zsh-users/zsh --filter tar.gz -d zsh.tar.gz
+	mkdir zsh-latest
+	tar -xf zsh.tar.gz -C zsh-latest --strip-components 1
+	cd zsh-latest || return
+	./configure >/dev/null
+	sudo make >/dev/null
+	sudo make install >/dev/null
+	sudo make clean >/dev/null
+	popd || return
+	ok
 }
 
 # install tmux plugin using tpm
 function install_tmux_plugin() {
-  action "Installing tmux plugins"
-  TP=$HOME/.config/tmux/plugins
-  TPM=$TP/tpm
-  clone_or_pull $TPM https://github.com/tmux-plugins/tpm.git
-  tmux new-session -d "sleep 5" && sleep 1
-  tmux source $HOME/.config/tmux/tmux.conf
-  $TPM/bin/install_plugins
-  print_success "finished installing tmux plugins"
-}
-
-# install zsh 5.8 from source
-function install_zsh_source() {
-  mkdir -p $1
-  info "installing zsh in $1"
-  pushd $1
-  require_yum ncurses-devel
-  curl -L https://www.zsh.org/pub/zsh-5.8.tar.xz -o zsh-5.8.tar.xz
-  tar -xf zsh-5.8.tar.xz
-  cd zsh-5.8
-  ./configure > /dev/null
-  sudo make > /dev/null
-  sudo make install > /dev/null
-  sudo make clean > /dev/null
-  popd
-  ok
+	mkdir -p plugins_dir
+	git clone --depth 1 https://github.com/tmux-plugins/tpm.git "$plugins_dir"/tpm
 }
 
 # install zsh plugin
 function install_zsh_plugin() {
-  action "Installing zsh plugins"
-  ZSH=$HOME/.config/zsh
-  mkdir -p $HOME/.cache/zsh
-  clone_or_pull $ZSH/zsh-syntax-highlighting \
-    https://github.com/zsh-users/zsh-syntax-highlighting.git
-  clone_or_pull $ZSH/zsh-history-substring-search \
-    https://github.com/zsh-users/zsh-history-substring-search.git
-  clone_or_pull $ZSH/powerlevel10k \
-    https://github.com/romkatv/powerlevel10k.git 
-  require_package autojump
-  print_success "finished installing zsh plugins"
+	git clone --depth 1 https://github.com/zsh-users/zsh-syntax-highlighting.git "$zsh_dir"/zsh/zsh-syntax-highlighting
+	git clone --depth 1 https://github.com/zsh-users/zsh-history-substring-search.git "$zsh_dir"/zsh/zsh-history-substring-search
+	git clone --depth 1 https://github.com/softmoth/zsh-vim-mode.git "$zsh_dir"/zsh/zsh-vim-mode
+	git clone --depth 1 https://github.com/romkatv/powerlevel10k.git "$zsh_dir"/zsh/powerlevel10k
 }
 
 # install homebrew for macos
 function install_brew() {
-  # Install Homebrew
-  bot "Time to start brewing"
-  running "checking homebrew..."
-  brew_bin=`hash brew` 2>&1 > /dev/null
-  
-  # Check if brew already installed
-  if [[ $? != 0 ]]; then
-    action "installing homebrew"
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    if [[ $? != 0 ]]; then
-      error "unable to install homebrew, script $0 abort!"
-      exit 2
-    fi
-    eval "$(/opt/homebrew/bin/brew shellenv)"
-    brew analytics off
-  else
-    ok
-    bot "Homebrew Found!"
-    read -r -p "run brew update && upgrade? [y|N] " response
-    if [[ $response =~ (y|yes|Y) ]]; then
-      action "updating homebrew..."
-      brew update
-      ok "homebrew updated"
-      action "upgrading brew packages..."
-      brew upgrade
-      ok "brews upgraded"
-    else
-      ok "skipped brew package upgrades."
-    fi
-  fi
+	# Install Homebrew
+	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+}
+
+function install_pack() {
+	# use $1 command to install packages in $2
+	eval $1 $(awk '!/^#/{ printf "%s ", $0 } END { print "" }' $2)
 }
