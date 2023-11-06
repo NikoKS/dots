e5d89b14fe07a3c0c6a8dd0baf511fd326e83898
NikoKS <nikolas.kur@gmail.com>
2023-11-06 17:10:25 +0700

    added codium.vim

--- dotfiles/astronvim/lua/user/lsp/formatting.lua
+++ dotfiles/astronvim/lua/user/lsp/formatting.lua
@@ -1,15 +1,15 @@
 return {
-  -- control auto formatting on save
-  format_on_save = {
-    enabled = true,      -- enable or disable format on save globally
-    ignore_filetypes = { -- disable format on save for specified filetypes
-      "python",
-    },
-  },
-  filter = function(client) -- fully override the default formatting function
-    if vim.bo.filetype == "svelte" or vim.bo.filetype == "astro" then
-      return client.name == "null-ls"
-    end
-    return true
-  end
+	-- control auto formatting on save
+	format_on_save = {
+		enabled = true, -- enable or disable format on save globally
+		ignore_filetypes = { -- disable format on save for specified filetypes
+			"python",
+		},
+	},
+	filter = function(client) -- fully override the default formatting function
+		if vim.bo.filetype == "svelte" or vim.bo.filetype == "astro" then
+			return client.name == "null-ls"
+		end
+		return true
+	end,
 }
--- dotfiles/astronvim/lua/user/plugins/user.lua
+++ dotfiles/astronvim/lua/user/plugins/user.lua
@@ -67,4 +67,29 @@ return {
 		end,
 	},
 	{ "jenterkin/vim-autosource", lazy = false },
+	{
+		"Exafunction/codeium.vim",
+		event = "User AstroFile",
+		config = function()
+			vim.keymap.set("i", "<S-CR>", function()
+				return vim.fn["codeium#Accept"]()
+			end, { expr = true })
+			vim.keymap.set("i", "<c-;>", function()
+				return vim.fn["codeium#CycleCompletions"](1)
+			end, { expr = true })
+			vim.keymap.set("i", "<c-,>", function()
+				return vim.fn["codeium#CycleCompletions"](-1)
+			end, { expr = true })
+			vim.keymap.set("i", "<c-x>", function()
+				return vim.fn["codeium#Clear"]()
+			end, { expr = true })
+			vim.keymap.set("n", "<leader>;", function()
+				if vim.g.codeium_enabled == true then
+					vim.cmd("CodeiumDisable")
+				else
+					vim.cmd("CodeiumEnable")
+				end
+			end, { noremap = true, desc = "Toggle Codeium active" })
+		end,
+	},
 }
